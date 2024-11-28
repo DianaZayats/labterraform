0195623d05efb2d58e87424aaacf7babd48da60f
Diana Zayats <marusya3651@gmail.com>
2024-11-28 21:45:50 +0200

    fixing

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,7 +101,7 @@ resource "aws_lightsail_container_service" "flask_app" {
 resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
   container {
     container_name = "flask-application"
-    image          = "${var.REPOSITORY_URI}:latest"
+    image = "${var.REPOSITORY_URI}/flask-application:latest"
     ports = {
       8080 = "HTTP"
     }
