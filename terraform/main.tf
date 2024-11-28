f64f951932a413471ee9dfe01c931ebc0ab1ed5f
Diana Zayats <marusya3651@gmail.com>
2024-11-28 22:00:33 +0200

    deleted ecr

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,11 +19,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Variable for repository URI
-variable "REPOSITORY_URI" {
-  type = string
-}
-
 # Data for existing security group
 data "aws_security_group" "existing_web_app" {
   filter {
@@ -78,50 +73,6 @@ resource "aws_instance" "webapp_instance" {
   tags = {
     Name = "webapp_instance"
   }
-}
-
-# AWS Lightsail Container Service
-resource "aws_lightsail_container_service" "flask_app" {
-  name  = "flask-app"
-  power = "nano"
-  scale = 1
-
-  private_registry_access {
-    ecr_image_puller_role {
-      is_active = true
-    }
-  }
-
-  tags = {
-    version = "1.0.0"
-  }
-}
-
-# Deployment version
-resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
-  container {
-    container_name = "flask-application"
-    image = "${var.REPOSITORY_URI}/flask-application:latest"
-    ports = {
-      8080 = "HTTP"
-    }
-  }
-
-  public_endpoint {
-    container_name = "flask-application"
-    container_port = 8080
-
-    health_check {
-      healthy_threshold   = 2
-      unhealthy_threshold = 2
-      timeout_seconds     = 2
-      interval_seconds    = 5
-      path                = "/"
-      success_codes       = "200-499"
-    }
-  }
-
-  service_name = aws_lightsail_container_service.flask_app.name
 }
 
 # Output the public IP of the instance
